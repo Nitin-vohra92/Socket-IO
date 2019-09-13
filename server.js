bfd1e33fd0e5a5fe4390159978e65e99a2867eb1
Nitin Vohra <nitinvohra92@gmail.com>
2019-09-13 23:16:31 +0530

    personal messages

--- server.js
+++ server.js
@@ -8,15 +8,37 @@ const app= express();
 const server = http.createServer(app);
 const io = socketio(server)
 
+let usersockets = {}
+
 app.use('/', express.static(path.join(__dirname,'frontend')))
 
 io.on('connection',(socket)=>{
     console.log("New socket formed from " + socket.id)
     socket.emit('connected')
+
+    socket.on('login', (data) => {
+        //username is on data.user
+        usersockets[data.user]=socket.id
+        console.log(usersockets)
+    })
+
+
     socket.on('send_msg',(data)=>{
         //if we use io.emit everyone gets it
         //if we use socket.broadcast.emit only others get it 
-        io.emit('recv_msg', data)
+       
+        if(data.message.startsWith('@')){
+            //data.message = "@a:hello"
+            let recipient = data.message.split(':')[0].substr(1);
+            let recptSocket = usersockets[recipient];
+            io.to(recptSocket).emit('recv_msg',data)
+        }
+        else{
+            socket.broadcast.emit('recv_msg',data)
+        }
+       
+       
+       // io.emit('recv_msg', data)
      //socket.broadcast.emit('recv_msg', data)
 
     })
